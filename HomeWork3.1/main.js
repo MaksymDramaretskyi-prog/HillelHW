0c71510fcd7edf2b54479dc066b1d067f511fdcb
Maksym Dramaretskyi <maksymdramaretskyi@Mac.home>
2026-02-26 21:20:22 +0100

    List of types

---
+++ HomeWork3.1/main.js
@@ -0,0 +1,18 @@
+//List of types
+// const arrTypeOf = [
+  // "String", 10, 100n, true, null, undefined, Symbol(), {}, null,
+// ];
+
+
+// for(let i = 0; i < arrTypeOf.length; i++ ){
+//   console.log(typeof arrTypeOf[i]);
+// }
+
+console.log(typeof "hello");
+console.log(typeof 10);
+console.log(typeof 100n);
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof Symbol());
+console.log(typeof {});
+console.log(typeof null);
