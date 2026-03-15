7a3230ff15369f0fd29e506e92f599b0fe57cdfe
Maksym Dramaretskyi <maksymdramaretskyi@Mac.home>
2026-03-15 13:39:11 +0100

    done HW6.3

--- HomeWork6.3/main.js
+++ HomeWork6.3/main.js
@@ -1,20 +1,36 @@
-const number = Number(prompt("Enter a positive integer:"));
+const teamListData = {
 
-if (!Number.isInteger(number) || number <= 1) {
-  console.log("Number must be an integer greater than 1.");
-} else {
-  let isPrime = true;
-
-  for (let i = 2; i < number; i++) {
-    if (number % i === 0) {
-      isPrime = false;
-      break;
+  juniorDevelopers:[
+    {
+      name: "Jack",
+      age: 19,
+      city: "London",
+      isAdmin: true,
+      email: "jack@gmail.com",
+      phone: "+44 123 456 789",
+      job: "developer",
+    },
+    {
+      name: "Pol",
+      age: 21,
+      city: "Oslo",
+      isAdmin: false,
+      email: "Pol@gmail.com",
+      phone: "+44 132 436 589",
+      job: "developer",
     }
-  }
+  ],
 
-  if (isPrime) {
-    console.log(`${number} is a prime number.`);
-  } else {
-    console.log(`${number} is NOT a prime number.`);
+  findJuniorDeveloper(name) {
+    const resultDev = teamListData.juniorDevelopers.find(juniorDevelopers => juniorDevelopers.name === name);
+    return resultDev ? resultDev : " Developer not found!"
+  },
+
+  addContactToList(name, age, email, phone) {
+    teamListData.juniorDevelopers.push({name, age, phone, email});
   }
 }
+ 
+teamListData.addContactToList("Dima", 100, "ojkfmeriofj3i", "+44 543 095 454");
+console.log(teamListData.findJuniorDeveloper("Maxim"));
+console.log(teamListData);
