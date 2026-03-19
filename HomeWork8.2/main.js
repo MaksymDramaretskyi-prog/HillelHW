a39a2057e8a7ee465f62305c3143e8b33bae4ade
Maksym Dramaretskyi <maksymdramaretskyi@Mac.home>
2026-03-19 17:41:12 +0100

    done HW8.2

--- HomeWork8.2/main.js
+++ HomeWork8.2/main.js
@@ -1,16 +1,9 @@
-const arrNumbers = [12, 24, 45, 23, 34, 0, 2, 0, 24, 23, 34]
-let deletnumber = 24;
+function name(a){
+  return function(b) {
+   return a * b;
+  };
+}
+
+console.log(name(5)(2));
 
 
-function removeElement(arrNumbers, deletnumber) {
-
-  for(let i = 0; i < arrNumbers.length; i++){
-    if(arrNumbers[i] === deletnumber){
-      arrNumbers.splice(i,1),
-      i--;
-    }
-  }
-  console.log(arrNumbers);
-};
-
-removeElement(arrNumbers, deletnumber);
