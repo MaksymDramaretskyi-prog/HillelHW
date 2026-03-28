2d168fe4276f4b6247b8ce17c9339ba506492eab
Maksym Dramaretskyi <maksymdramaretskyi@Mac.home>
2026-03-28 17:57:38 +0100

    done hm11.3

---
+++ HomeWork11.3/main.js
@@ -0,0 +1,20 @@
+/*Покласти в папку будь-які зображення 1.jpg, 2.jpg, 3.jpg, 4.jpg, 5.jpg, 6.jpg, 7.jpg, 8.jpg, 9.jpg. 
+Вивести зображення, отримане випадковим чином (Math.random)*
+*/
+const images = [
+  "img/1.jpg",
+  "img/2.jpg",
+  "img/3.jpg",
+  "img/4.jpg",
+  "img/5.jpg",
+  "img/6.jpg",
+  "img/7.jpg",
+  "img/8.jpg",
+  "img/9.jpg",
+  "img/10.jpg"
+];
+
+const img = document.querySelector("img");
+
+const randomIndex = Math.floor(Math.random() * images.length);
+img.src = images[randomIndex];
